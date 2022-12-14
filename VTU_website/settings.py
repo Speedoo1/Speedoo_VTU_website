3caab4e6ad09373b35a86fd3f4073b9016fbf70a
Speedoo <azeezadedeji638@gmail.com>
2022-12-14 11:33:42 +0100

    staticfile

--- VTU_website/settings.py
+++ VTU_website/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -128,8 +129,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static/'
 django_heroku.settings(locals())
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
@@ -144,4 +146,4 @@ MESSAGE_TAGS = {
 SESSION_COOKIE_AGE = 18000
 SESSION_SAVE_EVERY_REQUEST = True
 AUTH_USER_MODEL = 'index.profile'
-STATICFILES_DIRS = [BASE_DIR /'static']
+STATICFILES_DIRS = [BASE_DIR / 'static']
